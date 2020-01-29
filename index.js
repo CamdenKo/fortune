8e7a1676c6a8a6a2c06978655ccd90bfd686435d
Camden Ko <camdenko@gmail.com>
2020-01-29 02:05:51 -0600

    query processing

--- index.js
+++ index.js
@@ -1,53 +1,35 @@
-console.log('d')
-
-const decodeQuery = q =>
-  q.substring(2)
 
 const getElement = id =>
   document.getElementById(id)
 
 const showElement = (elem) => {
-  // elem.style.display = 'block'
   elem.style.opacity = 1
 }
+
+const decodeString = str =>
+  str
+    .split('%20')
+    .join(' ')
+
+const queryMsgToObj = msg =>
+  msg
+    .split('&')
+    .reduce((accum, cur) => {
+      const [l, r] = cur.split('=')
+      accum[l] = decodeString(r)
+      return accum
+    }, {})
+
+const processQuery = q =>
+  queryMsgToObj(q.substring(1))
 
 const hideElement = (elem) => {
   elem.style.display = 'none'
 }
 
-reloadWithQueryStringVars({"search": escape( $("#someId").val() )});
-
-function reloadWithQueryStringVars(queryStringVars) {
-  const existingQueryVars = location.search ? location.search.substring(1).split("&") : []
-  const currentUrl = location.search ? location.href.replace(location.search, "") : location.href
-  const newQueryVars = {}
-  const newUrl = currentUrl + "?"
-
-  if (existingQueryVars.length > 0) {
-    for (var i = 0; i < existingQueryVars.length; i++) {
-      var pair = existingQueryVars[i].split("=");
-      newQueryVars[pair[0]] = pair[1];
-    }
-  }
-  if (queryStringVars) {
-    for (var queryStringVar in queryStringVars) {
-      newQueryVars[queryStringVar] = queryStringVars[queryStringVar];
-    }
-  }
-  if (newQueryVars) {
-    for (var newQueryVar in newQueryVars) {
-      newUrl += newQueryVar + "=" + newQueryVars[newQueryVar] + "&";
-    }
-    newUrl = newUrl.substring(0, newUrl.length - 1);
-    window.location.href = newUrl;
-  } else {
-    window.location.href = location.href;
-  }
-}
-
 const main = () => {
   if (window.location.search) {
-    const msg = decodeQuery(window.location.search)
+    const msg = processQuery(window.location.search)
     console.log(msg)
   } else {
     showElement(getElement('cookieForm'))
---
+++ initial.js
@@ -0,0 +1,20 @@
+const combineQuery = queryArr =>
+  queryArr[0] + '=' + queryArr[1]
+
+const reloadWithQueryStringVars = (queryStringVars) => {
+  const currentUrl = location.href.includes('?') ?
+    location.href.split('?')[0] :
+    location.href
+
+  const toPostpend = [['fortune', document.getElementById('cookieInput').value]]
+  for (const param in queryStringVars) {
+    toPostpend.push([param, queryStringVars[param]])
+  }
+  const newUrl = currentUrl + '?' +
+    (toPostpend.reduce(
+      (a, b) => a + '&' + combineQuery(b),
+      '',
+    ).substring(1))
+  window.location.href = newUrl
+  return false
+}
